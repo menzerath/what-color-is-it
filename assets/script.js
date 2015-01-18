72c022a8ee1efd66acc23bfa5821617fdfa190df
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-01-18 19:49:04 +0100

    Added Web-App-Capability

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,13 @@ function updateScreen() {
 	var h = d.getHours();
 	var m = d.getMinutes();
 	var s = d.getSeconds();
-	
+
 	if (h < 10) { h = "0" + h };
 	if (m < 10) { m = "0" + m };
 	if (s < 10) { s = "0" + s };
-	
+
 	var hex = "#" + h + m + s;
-	
+
 	document.getElementById("time").innerHTML = (h + " : " + m + " : " + s);
 	document.getElementById("hex").innerHTML = hex;
 	document.body.style.background = hex;
