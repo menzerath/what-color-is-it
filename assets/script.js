40239a709e0c6ae10c44af3290d84185b9cd32a7
Marvin Menzerath <marvin.menzerath@gmx.de>
2015-06-02 19:18:41 +0200

    Removed Piwik-Tracking

--- assets/script.js
+++ assets/script.js
@@ -24,15 +24,4 @@ function updateScreen() {
 	}
 }
 
-var _paq = _paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-	var u="//piwik.menzerath.eu/";
-	_paq.push(['setTrackerUrl', u+'piwik.php']);
-	_paq.push(['setSiteId', 9]);
-	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-})();
-
 window.onload = setInterval(function () { updateScreen() }, 100);
